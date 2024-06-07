f25efcf8708267f4c9249aba69bb461a41e3160d
nsibiya023 <nsibiya023@student.wethinkcode.co.za>
2024-06-07 17:09:09 +0200

    DirectionTests

---
+++ src/test/java/za/co/wethinkcode/robotworlds/world/DirectionTest.java
@@ -0,0 +1,29 @@
+package za.co.wethinkcode.robotworlds.world;
+
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.robotworlds.Direction;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DirectionTest {
+    @Test
+    public void gettingRandomDirection(){
+        ArrayList<Direction> directions = new ArrayList<>();
+        directions.add(Direction.NORTH);
+        directions.add(Direction.EAST);
+        directions.add(Direction.SOUTH);
+        directions.add(Direction.WEST);
+
+        assertTrue(directions.contains(Direction.getRandomDirection()));
+    }
+
+    @Test
+    public void fromOrdinalException(){
+        assertThrows(IllegalArgumentException.class,
+                ()->{
+            Direction.fromOrdinal(7);
+                });
+    }
+}

1bfbb0671b86225818fd07b4314e43db18759db7
nbhengu023 <nbhengu023@student.wethinkcode.co.za>
2024-06-13 16:09:45 +0200

    README

--- src/main/java/za/co/wethinkcode/robotworlds/command/UnknownCommand.java
+++ src/main/java/za/co/wethinkcode/robotworlds/command/UnknownCommand.java
@@ -1,16 +1,16 @@
-package za.co.wethinkcode.robotworlds.command;
-
-import za.co.wethinkcode.robotworlds.world.IWorld;
-import za.co.wethinkcode.robotworlds.world.Robot;
-import za.co.wethinkcode.robotworlds.server.ServerResponse;
-
-public class UnknownCommand extends Command {
-    public UnknownCommand() {
-        super("unknown");
-    }
-
-    @Override
-    public ServerResponse execute(Robot target, IWorld world) {
-        return null;
-    }
-}
+//package za.co.wethinkcode.robotworlds.command;
+//
+//import za.co.wethinkcode.robotworlds.world.IWorld;
+//import za.co.wethinkcode.robotworlds.world.Robot;
+//import za.co.wethinkcode.robotworlds.server.ServerResponse;
+//
+//public class UnknownCommand extends Command {
+//    public UnknownCommand() {
+//        super("unknown");
+//    }
+//
+//    @Override
+//    public ServerResponse execute(Robot target, IWorld world) {
+//        return null;
+//    }
+//}

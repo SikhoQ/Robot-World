2083559611363ac7512346e4e0c146dfdc0b873c
nsibiya023 <nsibiya023@student.wethinkcode.co.za>
2024-06-14 11:58:31 +0200

    isPositionBlockedTest

--- src/test/java/za/co/wethinkcode/robotworlds/robot/SimpleBotTest.java
+++ src/test/java/za/co/wethinkcode/robotworlds/robot/SimpleBotTest.java
@@ -10,9 +10,16 @@ import za.co.wethinkcode.robotworlds.world.TextWorld;
 public class SimpleBotTest {
     IWorld world = new TextWorld();
     SimpleBot simpleBot = new SimpleBot("John", new Position(10, 20), Direction.NORTH,3389);
+
     @Test
     public void updatingPosition(){
         assertEquals("Done", simpleBot.updatePosition(10, world));
     }
 
+    @Test
+    public void isPositionBlocked(){
+        SimpleBot simpleBot = new SimpleBot("John", new Position(10, 20), Direction.NORTH,3389);
+        Position position = new Position(10, 20);
+        assertTrue(simpleBot.blocksPosition(position));
+    }
 }

ed465562c1557342acc63d6b22066024ffae30f1
nsibiya023 <nsibiya023@student.wethinkcode.co.za>
2024-06-14 11:50:45 +0200

    SimpleBotTest

---
+++ src/test/java/za/co/wethinkcode/robotworlds/robot/SimpleBotTest.java
@@ -0,0 +1,18 @@
+package za.co.wethinkcode.robotworlds.robot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import za.co.wethinkcode.robotworlds.Direction;
+import za.co.wethinkcode.robotworlds.Position;
+import za.co.wethinkcode.robotworlds.world.IWorld;
+import za.co.wethinkcode.robotworlds.world.TextWorld;
+
+public class SimpleBotTest {
+    IWorld world = new TextWorld();
+    SimpleBot simpleBot = new SimpleBot("John", new Position(10, 20), Direction.NORTH,3389);
+    @Test
+    public void updatingPosition(){
+        assertEquals("Done", simpleBot.updatePosition(10, world));
+    }
+
+}
